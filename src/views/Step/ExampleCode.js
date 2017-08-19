de5aa0cb89cf1afce5e16fad551b017267d5cfb1
FruitRaccoon <fruitraccoon@outlook.com>
2017-08-19 13:14:27 +1000

    Show red border around new lines

--- src/views/Step/ExampleCode.js
+++ src/views/Step/ExampleCode.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import { easeExpOut, easeExpInOut } from 'd3-ease';
+import { easeSinOut, easeExpInOut } from 'd3-ease';
 import NodeGroup from 'resonance/NodeGroup';
+
+const lineHeight = 17;
+
+function rTrim(s) {
+  return s.replace(/\s+$/, '');
+}
 
 const ExampleCode = ({ view, stepNo, items }) =>
   <div style={{ width: '90vw' }}>
@@ -10,46 +16,71 @@ const ExampleCode = ({ view, stepNo, items }) =>
         keyAccessor={d => d.key}
         start={(d, i) => ({
           opacity: d.diff.added ? 0 : 1,
-          y: `${d.initLine * 1.2}em`,
-          transform: `translate(${d.diff.added ? 1000 : 0})`,
+          translateX: d.diff.added ? 1000 : 0,
+          translateY: d.initLine * lineHeight,
+          rectScale: 50,
+          rectOpacity: 0,
         })}
         enter={(d, i) => [
           {
             opacity: d.diff.removed ? [0] : [1],
-            y: [`${d.compLine * 1.2}em`],
+            translateY: [d.compLine * lineHeight],
             timing: { duration: 1000, ease: easeExpInOut },
           },
           {
-            transform: ['translate(0)'],
-            timing: { delay: 500, duration: 2500, ease: easeExpOut },
+            translateX: [0],
+            timing: { delay: 500, duration: 2000, ease: easeSinOut },
+            rectScale: [1],
+          },
+          {
+            timing: { delay: 3000, duration: 500 },
+            rectOpacity: [1],
           },
         ]}
         update={(d, i) => [
           {
             opacity: 1,
-            y: `${d.compLine * 1.2}em`,
-            transform: 'translate(0)',
+            translateY: d.compLine * lineHeight,
+            translateX: 0,
+            rectScale: 1,
+            rectOpacity: 1,
           },
         ]}
         leave={(d, i) => [
           {
             opacity: 0,
+            rectOpacity: 0,
           },
         ]}>
         {nodes => {
           return (
             <g>
-              {nodes.filter(x => x.data.stepNo === stepNo).map(({ key, data: { diff }, state }) => {
+              {nodes.filter(x => x.data.stepNo === stepNo).map(node => {
+                const { key, data: { diff }, state } = node;
                 return (
-                  <text key={key} style={{ font: '14px monospace' }} {...state}>
-                    {diff.value.split('\n').map(l => l || ' ').map((l, i) => {
-                      return (
-                        <tspan key={i} x="0" dy="1.2em" xmlSpace="preserve">
+                  <g key={key}>
+                    <text
+                      transform={`translate(${state.translateX}, ${state.translateY})`}
+                      style={{ font: '14px monospace' }}
+                      opacity={state.opacity}
+                      ref={e => (node.bBox = e && diff.added && e.getBBox())}>
+                      {rTrim(diff.value).split('\n').map(l => l || ' ').map((l, i) =>
+                        <tspan key={i} x="0" dy={lineHeight} xmlSpace="preserve">
                           {l}
                         </tspan>
-                      );
-                    })}
-                  </text>
+                      )}
+                    </text>
+                    {node.bBox &&
+                      <rect
+                        transform={`translate(${node.bBox.x}, ${state.translateY + node.bBox.y})`}
+                        opacity={state.rectOpacity}
+                        stroke="red"
+                        strokeWidth="1px"
+                        fill="none"
+                        width={node.bBox.width}
+                        height={node.bBox.height}
+                      />}
+                  </g>
                 );
               })}
             </g>
